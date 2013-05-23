54b2ba0339c207e6eb97d56fcab1de9cc2a10ac6
Robert Forkel <xrotwang+gulp@googlemail.com>
2013-05-23 09:56:05 +0200

    fixes for deployment in jenkins

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -13,13 +13,13 @@ pip install --quiet pyramid simplejson docutils unittest2 Sphinx sphinxcontrib-h
 pip install --quiet nose nosexcover coverage pylint pep8 pyflakes
 pip install --quiet colander==0.9.8
 
-cd $WORKSPACE/
+cd $WORKSPACE/$1
 nosetests --with-xcoverage --with-xunit
 
-cd $WORKSPACE/
+cd $WORKSPACE/$1
 REV=`git rev-parse HEAD`
 sphinx-build -A revision=$REV -b html -d docs/_build/doctrees docs/source docs/_build/html
-sloccount --wide --details $WORKSPACE/$1 > $WORKSPACE/sloccount.sc
-pylint -f parseable --rcfile=$WORKSPACE/jenkins/pylintrc $1 | tee pylint.out
+sloccount --wide --details $WORKSPACE/$1/$1 > $WORKSPACE/sloccount.sc
+pylint -f parseable --rcfile=$WORKSPACE/$1/jenkins/pylintrc $1 | tee pylint.out
 pyflakes $1 | awk -F\: '{printf "%s:%s: [E]%s\n", $1, $2, $3}' | tee pylint-pyflakes.out
-pep8 --format=pylint --config=$WORKSPACE/jenkins/pep8 $1 | tee pep8.out
+pep8 --format=pylint --config=$WORKSPACE/$1/jenkins/pep8 $1 | tee pep8.out
